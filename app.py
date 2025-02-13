1ad865b3aea4e23d1e1c5b531a22c5aa1788c629
Gaurav <gaurav28.official@gmail.com>
2025-02-13 23:22:49 +0530

    i[dated

--- app.py
+++ app.py
@@ -1,8 +1,18 @@
 from fastapi import FastAPI
+from fastapi.middleware.cors import CORSMiddleware
 import asyncio
 from telegram_scraper import get_messages
 
 app = FastAPI()
+
+# Allow CORS for all origins (You can restrict it by modifying the origins list)
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=["*"],  # Allows all origins, you can specify your frontend URL here
+    allow_credentials=True,
+    allow_methods=["*"],  # Allows all HTTP methods
+    allow_headers=["*"],  # Allows all headers
+)
 
 @app.get("/")
 async def get_jobs():
